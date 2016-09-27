12edda248db3481ff5445f989b5d77c38847fccc
DHanley218003 <bassmaster965@yahoo.com>
2016-09-28 00:01:20 +0100

    It moves!

---
+++ Build 'N Crash/Assets/Scripts/DroneControls.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+var drone:Rigidbody;
+var speed:float = 90f;
+var turnSpeed:float = 5f;
+var hoverForce:float = 1.5f;
+var hoverHeight:float = 1f;
+var powerInput:float;
+var turnInput:float;
+var hoverInput:float;
+
+function Start () 
+{
+	drone = gameObject.GetComponent(Rigidbody) as Rigidbody;
+}
+
+function Update ()
+{
+	powerInput = Input.GetAxis ("Vertical");
+	turnInput = Input.GetAxis ("Horizontal");
+	hoverInput = Input.GetAxis ("Jump");
+	
+}
+
+function FixedUpdate () 
+{
+	hoverHeight += hoverInput;
+	if(drone.transform.position.y < hoverHeight - 3)
+		drone.AddForce(0f,hoverForce,0f,ForceMode.Force);
+		
+	else if(drone.transform.position.y > hoverHeight + 3)
+		drone.AddForce(0f,-hoverForce,0f,ForceMode.Force);
+		
+	//else
+		//drone.AddForce(0f,0f,0f,ForceMode.Force);
+	
+	drone.AddRelativeForce(0f, 0f, powerInput * speed);
+	drone.AddRelativeForce(0f, turnInput * turnSpeed, 0f);
+}
---
+++ Build 'N Crash/Assets/Scripts/Exit.js
@@ -0,0 +1,7 @@
+﻿#pragma strict
+
+function Update () 
+{
+	if(Input.GetKeyDown(KeyCode.Escape))
+		Application.Quit();
+}
