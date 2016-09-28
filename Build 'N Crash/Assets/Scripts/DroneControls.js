e998b9d3222f0c96a5edc928b08ed4fc8adafc0a
dhanley218003 <bassmaster965@yahoo.com>
2016-09-28 16:23:34 +0100

    Changed controls

--- Build 'N Crash/Assets/Scripts/DroneControls.js
+++ Build 'N Crash/Assets/Scripts/DroneControls.js
@@ -1,13 +1,14 @@
 ﻿#pragma strict
 var drone:Rigidbody;
 var speed:float = 20f;
-var turnSpeed:float = 15f;
+var turnSpeed:float = 14f;
 var hoverForce:float = 1.5f;
 var hoverHeight:float = 5f;
 var powerInput:float;
 var turnInput:float;
 var hoverInput:float;
 var pos:float;
+var tolerance:float = 1.5f;
 
 function Start () 
 {
@@ -26,17 +27,21 @@ function Update ()
 function FixedUpdate () 
 {
 	hoverHeight += hoverInput;
-	if(drone.transform.position.y < hoverHeight - 3)
+	if(drone.transform.position.y < hoverHeight - tolerance)
 	{
 		drone.AddForce(0f,hoverForce,0f);
 	}
-	else if(drone.transform.position.y > hoverHeight + 3)
+	else if(drone.transform.position.y > hoverHeight + tolerance)
 	{
 		drone.AddForce(0f,-hoverForce,0f);
 	}
 	//else
 		//drone.AddForce(0f,0f,0f,ForceMode.Force);
 	drone.AddRelativeForce(0f,hoverForce,0f);
-	drone.AddRelativeForce(0f, 0f, powerInput * speed);
-	drone.AddRelativeTorque(0f, turnInput * turnSpeed, 0f);
+	drone.AddRelativeForce(-(powerInput * speed), 0f, 0f);
+	drone.AddRelativeForce(0f, 0f, turnInput * turnSpeed);
+	if(Input.GetKey(KeyCode.Q))
+		drone.AddRelativeTorque(0f, -turnSpeed, 0f);
+	if(Input.GetKey(KeyCode.E))
+		drone.AddRelativeTorque(0f, turnSpeed, 0f);
 }
