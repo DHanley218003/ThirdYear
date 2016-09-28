68d02d00120f945252c39b3b7e27e2e8f7bb7aa6
dhanley218003 <bassmaster965@yahoo.com>
2016-09-28 12:11:57 +0100

    How does that make you sick?

--- Build 'N Crash/Assets/Scripts/DroneControls.js
+++ Build 'N Crash/Assets/Scripts/DroneControls.js
@@ -1,12 +1,13 @@
 ﻿#pragma strict
 var drone:Rigidbody;
-var speed:float = 90f;
-var turnSpeed:float = 5f;
+var speed:float = 20f;
+var turnSpeed:float = 15f;
 var hoverForce:float = 1.5f;
-var hoverHeight:float = 1f;
+var hoverHeight:float = 5f;
 var powerInput:float;
 var turnInput:float;
 var hoverInput:float;
+var pos:float;
 
 function Start () 
 {
@@ -17,22 +18,25 @@ function Update ()
 {
 	powerInput = Input.GetAxis ("Vertical");
 	turnInput = Input.GetAxis ("Horizontal");
-	hoverInput = Input.GetAxis ("Jump");
+	hoverInput = Input.GetAxiaas ("Jump");
 	
+	pos = drone.transform.position.y;
 }
 
 function FixedUpdate () 
 {
 	hoverHeight += hoverInput;
 	if(drone.transform.position.y < hoverHeight - 3)
-		drone.AddForce(0f,hoverForce,0f,ForceMode.Force);
-		
+	{
+		drone.AddForce(0f,hoverForce,0f);
+	}
 	else if(drone.transform.position.y > hoverHeight + 3)
-		drone.AddForce(0f,-hoverForce,0f,ForceMode.Force);
-		
+	{
+		drone.AddForce(0f,-hoverForce,0f);
+	}
 	//else
 		//drone.AddForce(0f,0f,0f,ForceMode.Force);
-	
+	drone.AddRelativeForce(0f,hoverForce,0f);
 	drone.AddRelativeForce(0f, 0f, powerInput * speed);
-	drone.AddRelativeForce(0f, turnInput * turnSpeed, 0f);
+	drone.AddRelativeTorque(0f, turnInput * turnSpeed, 0f);
 }
